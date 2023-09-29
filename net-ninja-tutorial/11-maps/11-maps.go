1e1f74dbfb02372aa9086a24b0df783d1e19ef62
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-09-30 00:01:43 +0200

    add maps episode of net ninja tutorial

---
+++ net-ninja-tutorial/11-maps/11-maps.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	menu := map[string]float64{
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"toffie pudding": 3.55,
+	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		312899432: "mario",
+		432432423: "luigi",
+		492304348: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[312899432])
+
+	phonebook[312899432] = "bowser"
+
+	fmt.Println(phonebook)
+}
