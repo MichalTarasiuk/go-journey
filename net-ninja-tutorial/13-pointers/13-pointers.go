abcba47a671d12adcabdb74c7f3d029370394abe
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-09-30 18:13:16 +0200

    add pointers episode of net ninja tutorial

---
+++ net-ninja-tutorial/13-pointers/13-pointers.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func updateName(x *string) {
+	*x = "wedge"
+}
+
+func main() {
+	name := "tifa"
+
+	// error
+	// updateName(name)
+	// fmt.Println("memory address of name is: ", &name)
+
+	m := &name
+	fmt.Println("memory address: ", m)
+	fmt.Println("value at memory address: ", *m)
+
+	fmt.Println(name)
+	updateName(m)
+	fmt.Println(name)
+}
+
+// |---name---|--pointer--|
+// |   0xe01  |  0x002    |
+// |   "tifa" |  pêx001   |
