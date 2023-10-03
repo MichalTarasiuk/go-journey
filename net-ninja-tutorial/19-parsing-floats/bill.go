5689b7ee00ac86259f08e148cf191c0be75db339
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-03 20:34:31 +0200

    add parsing floats episode of net ninja tutorial

---
+++ net-ninja-tutorial/19-parsing-floats/bill.go
@@ -0,0 +1,24 @@
+package main
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// make new bills
+func newBill(name string) bill {
+	return bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
